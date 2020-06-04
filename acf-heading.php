d9dbe2ccf2b42dd08527ddb0e20f26e61a86f1ff
Adam Patterson <hello@adampatterson.ca>
2020-06-04 13:20:00 -0600

    Initial Import

---
+++ acf-heading.php
@@ -0,0 +1,89 @@
+<?php
+
+/*
+Plugin Name: Advanced Custom Fields: Heading
+Plugin URI: https://www.adampatterson.ca
+Description: Adds headings to ACF Content
+Version: 1.0.0
+Author: Adam Patterson
+Author URI: https://www.adampatterson.ca
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+// exit if accessed directly
+if( ! defined( 'ABSPATH' ) ) exit;
+
+
+// check if class already exists
+if( !class_exists('adam_acf_plugin_heading') ) :
+
+class adam_acf_plugin_heading {
+	
+	// vars
+	var $settings;
+	
+	
+	/*
+	*  __construct
+	*
+	*  This function will setup the class functionality
+	*
+	*  @type	function
+	*  @date	17/02/2016
+	*  @since	1.0.0
+	*
+	*  @param	void
+	*  @return	void
+	*/
+	
+	function __construct() {
+		
+		// settings
+		// - these will be passed into the field class.
+		$this->settings = array(
+			'version'	=> '1.0.0',
+			'url'		=> plugin_dir_url( __FILE__ ),
+			'path'		=> plugin_dir_path( __FILE__ )
+		);
+		
+		
+		// include field
+		add_action('acf/include_field_types', 	array($this, 'include_field')); // v5
+		add_action('acf/register_fields', 		array($this, 'include_field')); // v4
+	}
+	
+	
+	/*
+	*  include_field
+	*
+	*  This function will include the field type class
+	*
+	*  @type	function
+	*  @date	17/02/2016
+	*  @since	1.0.0
+	*
+	*  @param	$version (int) major ACF version. Defaults to false
+	*  @return	void
+	*/
+	
+	function include_field( $version = false ) {
+		
+		// support empty $version
+		if( !$version ) $version = 4;
+		
+		// include
+		include_once('fields/class-adam-acf-field-heading-v' . $version . '.php');
+	}
+	
+}
+
+
+// initialize
+new adam_acf_plugin_heading();
+
+
+// class_exists check
+endif;
+	
+?>
